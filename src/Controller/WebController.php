ede8775c53da5b68350484b1d8dfe4fed7534ba4
emilio <emus4s@gmail.com>
2023-05-10 02:21:45 -0300

    Instalacion y configuracoin de formulario  de contacto

--- src/Controller/WebController.php
+++ src/Controller/WebController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,12 +19,28 @@ class WebController extends AbstractController
     }
 
     #[Route('/contacto', name: 'app_web_contact')]
-    public function conctact(): Response
+    public function conctact(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $nombre = $form->get('nombre')->getData();
+            $apellido = $form->get('apellido')->getData();
+            $telefono = $form->get('telefono')->getData();
+            $email = $form->get('email')->getData();
+            //dd($nombre);
 
-        return $this->render('web/primary.html.twig', [
-            'controller_name' => 'WebController',
+            #TODO: Enviar Email Consulta a Nova y enviar Email de Notificacion al Consultante sync
+
+            #Redireccinar al formualrio de consulta con mensaje succes
+            return $this->redirectToRoute('contact_succes');
+        }
+
+        return $this->render('web/contact.html.twig', [
+            'form' => $form,
         ]);
     }
 }
